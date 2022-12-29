0ce502802be6ca4d3924931fe1371440f92b258d
AndrewLester <alester220@gmail.com>
2022-12-28 21:53:39 -0500

    Redo styling

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 const emailReplacements = {
-    AT: '@',
-    DOT: '.',
+	AT: "@",
+	DOT: ".",
 };
-const emails = document.getElementsByClassName('email-link');
+const emails = document.getElementsByClassName("email-link");
 
 for (const email of emails) {
-    for (const replacer in emailReplacements) {
-        const replacement = emailReplacements[replacer];
-        email.href = email.href.replace(replacer, replacement);
-    }
+	for (const replacer in emailReplacements) {
+		const replacement = emailReplacements[replacer];
+		email.href = email.href.replace(replacer, replacement);
+	}
 }
 
-const header = document.getElementById('intro-header');
-const headerFixed = document.createElement('header');
-headerFixed.id = 'intro-header-fixed';
+const header = document.getElementById("intro-header");
+const headerFixed = document.createElement("header");
+headerFixed.id = "intro-header-fixed";
 headerFixed.innerHTML = header.innerHTML;
-const projects = document.getElementById('projects');
+const projects = document.getElementById("projects");
 
 const diff = window.scrollY - projects.offsetTop;
 headerFixed.hidden = diff <= 0;
@@ -23,16 +23,16 @@ headerFixed.hidden = diff <= 0;
 document.body.prepend(headerFixed);
 
 window.onscroll = () => {
-    const diff = window.scrollY - projects.offsetTop;
-    headerFixed.classList.toggle('fixed', diff > 0);
-    if (diff > 0) {
-        headerFixed.style.transform = `translateY(${Math.min(
-            0,
-            diff - headerFixed.clientHeight
-        )}px)`;
-        headerFixed.hidden = false;
-    } else {
-        headerFixed.style.transform = '';
-        headerFixed.hidden = true;
-    }
+	const diff = window.scrollY - projects.offsetTop;
+	headerFixed.classList.toggle("fixed", diff > 0);
+	if (diff > 0) {
+		headerFixed.style.transform = `translateY(${Math.min(
+			0,
+			diff - headerFixed.clientHeight
+		)}px)`;
+		headerFixed.hidden = false;
+	} else {
+		headerFixed.style.transform = "";
+		headerFixed.hidden = true;
+	}
 };
